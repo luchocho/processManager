f4f5bc1fd888e11070293b0407aaafdc2f6a39d8
Luchocho <luchocho@gmail.com>
2017-11-11 15:46:42 +0100

    prueba conexion

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require("express"),
 app.locals.moment = require('moment');
 
 // mongoose.connect("mongodb://localhost/todo_app");
-mongoose.connect("mongodb://process.env.userdb:process.env.passdb@cluster0-shard-00-00-cnrcg.mongodb.net:27017,cluster0-shard-00-01-cnrcg.mongodb.net:27017,cluster0-shard-00-02-cnrcg.mongodb.net:27017/todo_app?&replicaSet=Cluster0-shard-0&authSource=admin", {
+mongoose.connect("mongodb://process.env.userdb:process.env.passdb@cluster0-shard-00-00-cnrcg.mongodb.net:27017,cluster0-shard-00-01-cnrcg.mongodb.net:27017,cluster0-shard-00-02-cnrcg.mongodb.net:27017/todo_app?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", {
   useMongoClient: true
 });
 app.use(express.static('public'));
